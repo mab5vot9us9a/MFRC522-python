c308f83b479e0290ab3ec7e6feaf5e5190621dc5
Maurice Fahn <maurice.fahn@gmail.com>
2016-07-06 22:24:18 +0200

    Coloring for All purpose bit.

--- MFRC522.py
+++ MFRC522.py
@@ -113,7 +113,7 @@ class MFRC522:
     __colored_print__ = {
         0: __default_block_print__.format(color=tcolors.RED_BRIGHT, end=tcolors.ENDC),
         1: __default_block_print__.format(color=tcolors.BLUE_BRIGHT, end=tcolors.ENDC),
-        2: __trailer_block_print__.format(color_A=tcolors.GREEN_BRIGHT, color_AB=tcolors.PURPLE_BRIGHT, color_P=tcolors.GRAY_22 color_B=tcolors.ORANGE_BRIGHT, end=tcolors.ENDC),
+        2: __trailer_block_print__.format(color_A=tcolors.GREEN_BRIGHT, color_AB=tcolors.PURPLE_BRIGHT, color_P=tcolors.GRAY_22, color_B=tcolors.ORANGE_BRIGHT, end=tcolors.ENDC),
         3: __default_block_print__.format(color=tcolors.ENDC, end="")
     }
 
