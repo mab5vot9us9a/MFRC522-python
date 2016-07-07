1a27a34b6b6cbacef43a60bad40bde0635c32acb
Maurice Fahn <maurice.fahn@gmail.com>
2016-07-07 18:24:24 +0200

    MOD tmp

--- tmp.py
+++ tmp.py
@@ -6,8 +6,9 @@ import MFRC522
 MIFAREReader = MFRC522.MFRC522()
 MIFAREReader.WriteAll(ord("A"))
 print("Wrote " + str(ord("A")))
+
+(status, TagType) = MIFAREReader.Request(MIFAREReader.PICC_REQIDL)
 if status == MIFAREReader.MI_OK:
-    (status, TagType) = MIFAREReader.Request(MIFAREReader.PICC_REQIDL)
     (status, uid) = MIFAREReader.Anticoll()
     MIFAREReader.SelectTag(uid)
     key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
